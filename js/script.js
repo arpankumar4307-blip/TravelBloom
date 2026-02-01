585827affb1b906d8f6ae442eb8067ed361b01f4
arpankumar4307-blip <arpankumar4307@gmail.com>
2026-02-01 18:59:30 +0530

    Add search functionality for places

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const data = {
+    beach: [
+        { name: "Maldives Beach", img: "images/beach1.jpg" },
+        { name: "Bali Beach", img: "images/beach2.jpg" }
+    ],
+    temple: [
+        { name: "Golden Temple", img: "images/temple1.jpg" },
+        { name: "Angkor Wat", img: "images/temple2.jpg" }
+    ],
+    country: [
+        { name: "Switzerland", img: "images/country1.jpg" },
+        { name: "Japan", img: "images/country2.jpg" }
+    ]
+};
+
+function searchPlaces() {
+    const input = document.getElementById("searchInput").value.toLowerCase();
+    const results = document.getElementById("results");
+    results.innerHTML = "";
+
+    let places = [];
+
+    if (input.includes("beach")) places = data.beach;
+    else if (input.includes("temple")) places = data.temple;
+    else if (input.includes("country")) places = data.country;
+    else {
+        results.innerHTML = "<h3>No recommendations found. Try beach, temple, or country.</h3>";
+        return;
+    }
+
+    places.forEach(place => {
+        results.innerHTML += `
+            <div class="card">
+                <img src="${place.img}" alt="${place.name}">
+                <h3>${place.name}</h3>
+            </div>
+        `;
+    });
+}
